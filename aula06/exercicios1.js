acb002b0234f4ae21aef180399f6b7191b896a07
YuriSouza07 <yurideoliveirasouza07@gmail.com>
2025-10-06 19:16:46 -0300

    Create exercicios1.js

---
+++ aula06/exercicios1.js
@@ -0,0 +1,10 @@
+//Crie um programa que utilize um loop while para exibir os números de 1 a 20 na tela. 
+let num = 1;
+while(num<=20){
+    console.log("exibir com while..", num);
+    i++ // exibir numeoros
+}
+console.log('-=-=-=-=-=-=-=-=-=-=-=-');
+for(let num=1; num<=20; num++){
+    console.log("exibir com for..", num);
+}
