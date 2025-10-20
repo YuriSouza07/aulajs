f04dcf39adb44596f21bd22a974556d1e6f912fd
YuriSouza07 <yurideoliveirasouza07@gmail.com>
2025-10-20 20:57:39 -0300

    aula12

---
+++ aula12/exec2.js
@@ -0,0 +1,11 @@
+//2.Peça ao usuário para inserir 4 números. Armazene em um array e exiba a média.
+ let numeros = []; 
+ let soma = 0;
+    for (let i = 0; i < 4; i++){ 
+        let n = Number(prompt('Digite o número'));
+        numeros.push(n);
+        soma = soma + n;
+} 
+let media = soma/numeros.length;
+console.log('A média é: ' + media);
+console.log('O conteúdo do array: ' + numeros);
